4d8aadda039daa458ae0a96c62a8033d65c9ef0f
Ravi Ranjan <31163976+RAVIRANJAN123456789@users.noreply.github.com>
2019-06-10 21:33:34 +0530

    First n fibonacci numbers!

---
+++ fib.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int fibonacci(int n)
+{
+  if(n==0)
+  {
+    return 0;
+  }
+  else if(n==1)
+  {
+      return 1;
+  }
+  else{
+    return fibonacci(n-1)+fibonacci(n-2);
+
+  }
+}
+int main()
+{
+    int n;int i;
+    printf("\n enter the count till which fibonaaci numbers are to be printed \n");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("%d\n",fibonacci(i));
+    }
+    return 0;
+}
